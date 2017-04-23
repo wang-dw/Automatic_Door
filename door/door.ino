bdbcb40199b80cee622da122fe2d1b3368e6adfa
wang-dw <wangdw@miamioh.edu>
2017-04-22 20:01:19 -0400

    code

--- door/door.ino
+++ door/door.ino
@@ -1,4 +1,5 @@
 #include <Stepper.h>
+#include <elapsedMillis.h>
 
 #define STEPS_PER_MOTOR_REVOLUTION 32
 #define STEPS_PER_OUTPUT_REVOLUTION 32 * 64  //2048
@@ -6,6 +7,9 @@
 Stepper small_stepper(STEPS_PER_MOTOR_REVOLUTION, 8, 10, 9, 11);
 
 int  Steps2Take;
+
+const int trigPin = 6;
+const int echoPin = 7;
 
 int lockPin = 12;
 int pirPin = 13;
@@ -25,10 +29,39 @@ void setup() {
 
   pinMode(pirPin, INPUT);
   pinMode(lockPin, INPUT);
+  pinMode(echoPin, INPUT);
   STATE = idle;
+
+  Serial.begin(9600);
 }
 
 void loop() {
+
+  long duration, inches, cm;
+
+  pinMode(trigPin, OUTPUT);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+
+  duration = pulseIn(echoPin, HIGH);
+
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+
+  if (duration < 23200) { //23200 = 4m
+    Serial.print(inches);
+    Serial.print("in, ");
+    Serial.print(cm);
+    Serial.print("cm");
+    Serial.println();
+  }
+  else {
+    Serial.print("Out of Range");
+  }
 
   pirValue = digitalRead(pirPin);
   lockValue = digitalRead(lockPin);
@@ -36,13 +69,13 @@ void loop() {
 
   doorState();
 
-  //delay(1000);
+  delay(200);
 }
 
-void doorState()
-{
-  switch (STATE) //CHANGE STATES
-  {
+void doorState() {
+
+  switch (STATE) { //CHANGE STATES
+
     case idle:
       if (lockValue == HIGH)
         STATE = lockDoor;
@@ -81,8 +114,8 @@ void doorState()
       break;
   }
 
-  switch (STATE) //STATE FUNCTIONS
-  {
+  switch (STATE) { //STATE FUNCTIONS
+
     case idle:
       delay(200);
       break;
@@ -110,5 +143,21 @@ void doorState()
 
   }
 
+}
+
+long microsecondsToInches(long microseconds) {
+  // According to Parallax's datasheet for the PING))), there are
+  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+  // second).  This gives the distance travelled by the ping, outbound
+  // and return, so we divide by 2 to get the distance of the obstacle.
+  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
 }
 
